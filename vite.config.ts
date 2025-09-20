a41c33a6fbe2ef8917a00539b12607ed4bf32db1
shivani patel <kshivani112255@gmail.com>
2025-09-21 00:31:20 +0530

    fix: vite base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // 👈 yeh add kiya hai
+  base: '/', // 👈 yeh add kiya hai
   resolve: {
     extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
     alias: {
